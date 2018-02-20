65ff3b7dd3c41dc1cf69bb4f98969e300abd760e
Mateusz Piskorz <mattpiskorz@gmail.com>
2018-02-20 10:14:45 +0000

    fixed es lint issues

--- js/db/npcs.js
+++ js/db/npcs.js
@@ -1,11 +1,19 @@
 export default {
-    "demo_island" : [
-        {
-            id:"angry_monk",
-            name: "Angry Monk",
-            x: 25,
-            y: 25,
-            image: "assets/angry_monk.png"
-        }
-    ]
-}
+  'demo_island' : [
+    {
+      id:'angry_monk',
+      name: 'Angry Monk',
+      x: 25,
+      y: 25,
+      image: 'assets/angry_monk.png'
+    },
+    {
+      id:'angry_monk2',
+      name: 'Angry Monk',
+      x: 25,
+      y: 25,
+      image: 'assets/angry_monk.png'
+    }
+
+  ]
+};
--- js/npc.js
+++ js/npc.js
@@ -3,34 +3,34 @@ import SpriteImage from './sprite_image';
 
 
 export default class NPC {
-    constructor(data) {
-      this.id = data.id;
-      this.name = data.name;
-      this.image_src = data.image;
-      this.image = new SpriteImage(this.getSprite(), 0, 0);
-      this.position = new Position(data.x, data.y);
-      this.x = data.x;
-      this.y = data.y;
-    }
-  
-  
-    position() {
-      return this.position;
-    }
-  
-    moveTo(destinationPosition) {
-      this.position.setTo(destinationPosition);
-    }
-  
-    moveToMap(mapId) {
-      this.map = mapId;
-    }
-  
-    getSprite() {
-      var image = new Image(32, 32);
-      image.src = this.image_src;
-      return image;
-    }
-
+  constructor(data) {
+    this.id = data.id;
+    this.name = data.name;
+    this.image_src = data.image;
+    this.image = new SpriteImage(this.getSprite(), 0, 0);
+    this.position = new Position(data.x, data.y);
+    this.x = data.x;
+    this.y = data.y;
   }
+  
+  
+  position() {
+    return this.position;
+  }
+  
+  moveTo(destinationPosition) {
+    this.position.setTo(destinationPosition);
+  }
+  
+  moveToMap(mapId) {
+    this.map = mapId;
+  }
+  
+  getSprite() {
+    var image = new Image(32, 32);
+    image.src = this.image_src;
+    return image;
+  }
+
+}
   
